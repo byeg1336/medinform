2bbcd35332a6931dfd8ee5a54b0ace99d45f69f4
byeg1336 <byeg1336@gmail.com>
2025-04-22 00:25:49 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,27 @@
+'Привет, Java!'
+var myThing;
+myThing=5;
+mything="это строка";
+var numberNine= 9;
+var stringNine ="9";
+numberNine + numberNine;
+stringNine + stringNine;
+var greeting = "Привет, ";
+var myName = "Ник";
+greeting + myName
+myName.length
+
+
+let name_orz = "ОРЗ";
+let name_chickenpox = "ветрянка";
+
+
+let num_orz_mon = 2;   // Понедельник
+let num_orz_tue = 3;   // Вторник
+let num_orz_wed = 2;   // Среда
+let num_chickenpox_tue = 1; // Вторник
+let num_chickenpox_wed = 4; // Среда
+
+
+let num_orz_week = num_orz_mon + num_orz_tue + num_orz_wed;
+let num_chickenpox_week = num_chickenpox_tue + num_chickenpox_wed;
